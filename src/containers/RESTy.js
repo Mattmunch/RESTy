91463f2d42048c72cd0a19335a0b78801e411b41
Matt Munch <mattmunch94@gmail.com>
2020-01-30 17:28:50 -0800

    history and everything functioning

--- src/containers/RESTy.js
+++ src/containers/RESTy.js
@@ -3,6 +3,7 @@ import Form from '../components/form/Form';
 import HistoryList from '../components/history/HistoryList';
 import ResponseDisplay from '../components/responseDisplay/ResponseDisplay';
 import { makeFetch } from '../services/makeFetch';
+import styles from './RESTy.css';
 
 export default class RESTy extends Component {
   state = {
@@ -12,10 +13,14 @@ export default class RESTy extends Component {
     response: [],
     historyItems: []
   }
-
     handleSubmit = event => {
       event.preventDefault();
       console.log(this.state);
+      this.setState(prevState => ({
+        historyItems: [
+          ...prevState.historyItems,
+          { url: this.state.url }
+        ] }));
       makeFetch(this.state.url)
         .then(res => {
           this.setState({ response: res });
@@ -28,7 +33,7 @@ export default class RESTy extends Component {
     const { url, method, requestBody, response, historyItems } = this.state;
 
     return (
-      <main>
+      <main className={styles.Container}>
         <Form onSubmit={this.handleSubmit} onChange={this.handleChange} url={url} method={method} requestBody={requestBody}  />
         <HistoryList historyItems={historyItems} />
         <ResponseDisplay response={response} />
